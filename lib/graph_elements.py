a120b1133721d160e906c1f4068fa75c7c99a480
Lukas Pirl <git@lukas-pirl.de>
2017-07-03 17:01:52 +0200

    moved code from Vertex to GolfGraph

--- lib/graph_elements.py
+++ lib/graph_elements.py
@@ -2,38 +2,23 @@
 All elements of a graph.
 """
 
-from itertools import chain
-
 
 
 class Vertex(object):
   """
   A node in a graph.
+  Keep this minimal.
   """
 
-  def __init__(self, id, num_ports):
+  def __init__(self, id):
     self.id = id
-    self.num_ports = num_ports
-    self._related = set()
+    self.related = list()
 
   def __hash__(self):
     return self.id
 
-  def __lt__(self, other):
-    return self.id < other.id
-
   def __eq__(self, other):
     return self.id == other.id
-
-  def add_related_bidirectional(self, other):
-    assert isinstance(other, Vertex)
-    self._related.add(other)
-    other._related.add(self)
-    assert len(self._related) <= self.num_ports
-
-  @property
-  def related(self):
-    return self._related
 
 
 
@@ -46,7 +31,12 @@ class GolfGraph(object):
   def __init__(self, order, degree):
     self.order = order
     self.degree = degree
-    self.vertices = [Vertex(i, degree) for i in range(order)]
+
+    self.vertices = [Vertex(i) for i in range(order)]
 
   def add_edge(self, vertex_a, vertex_b):
-    self.vertices[vertex_a.id].add_related_bidirectional(vertex_b)
+    assert vertex_a != vertex_b
+    vertex_a.related.append(vertex_b)
+    vertex_b.related.append(vertex_a)
+    assert len(vertex_a.related) <= self.degree
+    assert len(vertex_b.related) <= self.degree
--- test/graph_elements_test.py
+++ test/graph_elements_test.py
@@ -2,31 +2,31 @@ from test import BaseTest
 
 from lib.graph_elements import GolfGraph, Vertex
 
-class VertexTest(BaseTest):
-    """
-    Tests class ``Vertex``.
-    """
+#~ class VertexTest(BaseTest):
+    #~ """
+    #~ Tests class ``Vertex``.
+    #~ """
 
-    def setUp(self):
-        self.v1 = Vertex(1, 1)
-        self.v2 = Vertex(2, 1)
-        self.v3 = Vertex(3, 1)
+    #~ def setUp(self):
+        #~ self.v1 = Vertex(1)
+        #~ self.v2 = Vertex(2)
+        #~ self.v3 = Vertex(3)
 
-    def test_order(self):
-        """
-        Tests whether nodes of edges are ordered.
-        """
-        self.v1.add_related_bidirectional(self.v2)
-        self.assertNotEqual(self.v1, self.v2)
-        self.assertEqual(self.v1, self.v2.related.pop())
+    #~ def test_order(self):
+        #~ """
+        #~ Tests whether nodes of edges are ordered.
+        #~ """
+        #~ self.v1.add_edge(self.v2)
+        #~ self.assertNotEqual(self.v1, self.v2)
+        #~ self.assertEqual(self.v1, self.v2.related.pop())
 
-    def test_num_ports(self):
-        """
-        Tests whether nodes of edges are ordered.
-        """
-        self.v1.add_related_bidirectional(self.v2)
-        with self.assertRaises(AssertionError):
-          self.v1.add_related_bidirectional(self.v3)
+    #~ def test_num_ports(self):
+        #~ """
+        #~ Tests whether nodes of edges are ordered.
+        #~ """
+        #~ self.v1.add_edge(self.v2)
+        #~ with self.assertRaises(AssertionError):
+          #~ self.gra.v1.add_edge(self.v3)
 
 
 class GolfGraphTest(BaseTest):
@@ -41,11 +41,37 @@ class GolfGraphTest(BaseTest):
 
     def test_graph_creation(self):
         """
-        Tests if all nodes are created correctly.
+        Tests whether all vertices are created correctly.
         """
 
         # all initial nodes
         vertices = self.graph.vertices
         degree = self.degree
         for i in range(self.order):
-          self.assertIn(Vertex(i, degree), vertices)
+          self.assertIn(Vertex(i), vertices)
+
+    def test_degree_invariant(self):
+        """
+        Tests whether we cannot add more edges than ``self.degree``.
+        """
+        graph = self.graph
+        vertices = graph.vertices
+        add_edge = graph.add_edge
+        vertex_to_add_to = vertices[-1]
+
+        assert len(vertices) > self.degree
+
+        # do what is allowed
+        for i in range(self.degree):
+          add_edge(vertex_to_add_to, vertices[i])
+
+        with self.assertRaises(AssertionError):
+          add_edge(vertex_to_add_to, vertices[-2])
+
+    def test_no_self_edges(self):
+        """
+        Tests whether we cannot add an edges between a vertex and itself.
+        """
+        vertex = self.graph.vertices[0]
+        with self.assertRaises(AssertionError):
+          self.graph.add_edge(vertex, vertex)
